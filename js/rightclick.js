000964387634ddb8872c7b00fc4927b3ff042cbd
Vartikaj <38675130+Vartikaj@users.noreply.github.com>
2021-01-18 20:45:10 +0530

    Update rightclick.js

--- js/rightclick.js
+++ js/rightclick.js
@@ -2,3 +2,12 @@ window.addEventListener('contextmenu', function (e) {
   alert('Right-click is disabled');
   e.preventDefault();
 }, false);
+
+// $(document).bind("contextmenu",function(e) {
+//  e.preventDefault();
+// });
+// $(document).keydown(function(e){
+//     if(e.which === 123){
+//        return false;
+//     }
+// });
